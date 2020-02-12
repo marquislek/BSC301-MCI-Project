3b19ae3a0e36c140d8dad3b9548bd4c45eda4a3d
marquislek <marquislek@gmail.com>
2020-02-12 10:22:20 +0800

    Updates

---
+++ precache-manifest.7d2b01e13f1ed41a9e8c2fa2d9789939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6c64d0b4998a69ccfd568d7b5345d5",
+    "url": "/bsc301-mci-project/index.html"
+  },
+  {
+    "revision": "fc20a7c11f69f638f7a9",
+    "url": "/bsc301-mci-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3bd7ab842fd704f2382",
+    "url": "/bsc301-mci-project/static/js/2.724443ed.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bsc301-mci-project/static/js/2.724443ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc20a7c11f69f638f7a9",
+    "url": "/bsc301-mci-project/static/js/main.057ec07a.chunk.js"
+  },
+  {
+    "revision": "34e0f4704b6fedabc6ec",
+    "url": "/bsc301-mci-project/static/js/runtime-main.297603e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bsc301-mci-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
